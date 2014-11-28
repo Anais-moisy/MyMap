20c972a91a366b25a3f971b3f3f8030ab3d25194
Chris Barker <admin@local.host>
2014-11-28 14:18:19 +0000

    focus map, case for first cutout'

--- www/js/map.js
+++ www/js/map.js
@@ -61,9 +61,16 @@ var geolocationSuccess = function(position)
 
 	myOverlay.setPaths(currentPaths);*/
 
+	/* lets see the new point */
+	focusMap();
+
 	addPointToRoute( position.coords );
 
-	console.log (storageObj);
+	/* if its the first rectangle */
+	if( storageObj.routes[routeIndex].points.length <= 1 )
+	{
+		createOverlayShape( p, 1.0, 1.0 );
+	}
 };
 
 /* Handle errors with gps */
@@ -73,9 +80,15 @@ function geolocationError(error)
 		'message: ' + error.message + '\n');
 }
 
+function focusMap ()
+{
+	map.setZoom(16);
+	map.setCenter( new google.maps.LatLng( p.coords.latitude, p.coords.longitude ) );
+}
+
 
 /* Make a new rect (circle later?) for our new location */
-function createOverlayShape( point_array, scaleX, scaleY )
+function createOverlayShape( p, scaleX, scaleY )
 {
 	return new google.maps.MVCArray([
 		new google.maps.LatLng((+p.coords.latitude + (offset*scaleY) ), (+p.coords.longitude - (offset*scaleX*2))),
